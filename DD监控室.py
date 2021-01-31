4d598565fad2a1b71f36ce893fde394e91abe46c
woclass <inkydragon@users.noreply.github.com>
2021-01-31 14:53:14 +0800

    [gui/close] 直接调用close方法，避免多次关闭player

--- DD监控室.py
+++ DD监控室.py
@@ -676,12 +676,11 @@ class MainWindow(QMainWindow):
         self.layoutSettingPanel.close()
         self.liverPanel.addLiverRoomWidget.close()
         for videoWidget in self.videoWidgetList:
-            videoWidget.playerFree()  # 销毁实例
             videoWidget.getMediaURL.recordToken = False  # 关闭缓存并清除
             videoWidget.getMediaURL.checkTimer.stop()
             videoWidget.checkPlaying.stop()
+            videoWidget.close()
         for videoWidget in self.popVideoWidgetList:  # 关闭悬浮窗
-            videoWidget.playerFree()
             videoWidget.getMediaURL.recordToken = False
             videoWidget.getMediaURL.checkTimer.stop()
             videoWidget.checkPlaying.stop()
